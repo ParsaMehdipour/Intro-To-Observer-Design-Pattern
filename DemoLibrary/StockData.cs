0dadcdbdf0a714cf2de1a6a2cb03eed10d344c49
Parsa <mehdipourparsa@gmail.com>
2024-10-23 18:28:02 -0400

    feat: stock data model

---
+++ DemoLibrary/StockData.cs
@@ -0,0 +1,19 @@
+﻿namespace DemoLibrary;
+
+/// <summary>
+/// Data structure for stock information
+/// </summary>
+public class StockData
+{
+    public string Symbol { get; }
+    public decimal Price { get; }
+    public DateTime LastUpdate { get; }
+    public decimal PriceChange { get; }
+
+    public StockData(string symbol, decimal price, DateTime lastUpdate)
+    {
+        Symbol = symbol;
+        Price = price;
+        LastUpdate = lastUpdate;
+    }
+}
