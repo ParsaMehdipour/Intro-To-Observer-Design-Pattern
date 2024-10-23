659b030309c24ae629ef5985a52a7a54bcfb3566
Parsa <mehdipourparsa@gmail.com>
2024-10-23 18:28:09 -0400

    feat: client code

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,28 @@
+﻿// Create the stock exchange
+using DemoLibrary.ConcreteObservers;
+using DemoLibrary.ConcretePublishers;
+
+var stockExchange = new StockExchange();
+
+// Add some stocks
+stockExchange.AddStock("AAPL", 150.00m);
+stockExchange.AddStock("GOOGL", 2800.00m);
+
+// Create investors
+var individualInvestor = new IndividualInvestor("John Doe", 5.00m);
+var investmentFund = new InvestmentFund("Growth Fund", 1000000.00m);
+
+// Subscribe investors to stock updates
+stockExchange.Subscribe("AAPL", individualInvestor);
+stockExchange.Subscribe("AAPL", investmentFund);
+stockExchange.Subscribe("GOOGL", investmentFund);
+
+// Simulate some stock price changes
+stockExchange.UpdateStockPrice("AAPL", 152.50m);
+stockExchange.UpdateStockPrice("GOOGL", 2850.00m);
+
+// Unsubscribe an investor
+stockExchange.Unsubscribe("AAPL", individualInvestor);
+
+// Another price change
+stockExchange.UpdateStockPrice("AAPL", 153.75m);
